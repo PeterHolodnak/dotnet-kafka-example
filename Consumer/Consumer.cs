a22d03da59b2fc254bbcf9b7efcb6fc492498e2e
Peter Holodňák <peter.holodnak@gmail.com>
2024-01-01 18:22:12 +0100

    init dotnet kafka project

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddInMemoryCollection()
+    .Build();
+
+configuration["bootstrap.servers"] = "kafka:9092";
+configuration["group.id"] = "kafka-dotnet-getting-started";
+configuration["auto.offset.reset"] = "earliest";
+
+const string topic = "purchases";
+
+CancellationTokenSource cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true; // prevent the process from terminating.
+    cts.Cancel();
+};
+
+using var consumer = new ConsumerBuilder<string, string>(configuration.AsEnumerable()).Build();
+
+consumer.Subscribe(topic);
+
+try
+{
+    while (true)
+    {
+        var cr = consumer.Consume(cts.Token);
+        Console.WriteLine($"Consumed event from topic {topic}: key = {cr.Message.Key,-10} value = {cr.Message.Value}");
+    }
+}
+catch (OperationCanceledException)
+{
+    // Ctrl-C was pressed.
+}
+finally
+{
+    consumer.Close();
+}
---
+++ Producer/Producer.cs
@@ -0,0 +1,42 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddInMemoryCollection()
+    .Build();
+
+configuration["bootstrap.servers"] = "kafka:9092";
+
+const string topic = "purchases";
+const int messagesToProduceCount = 10;
+
+string[] users = { "eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther" };
+string[] items = { "book", "alarm clock", "t-shirts", "gift card", "batteries" };
+
+using var producer = new ProducerBuilder<string, string>(configuration.AsEnumerable()).Build();
+
+var producedMessagesCount = 0;
+
+for (int i = 0; i < messagesToProduceCount; ++i)
+{
+    var user = users[Random.Shared.Next(users.Length)];
+    var item = items[Random.Shared.Next(items.Length)];
+
+    producer.Produce(topic, new Message<string, string> { Key = user, Value = item },
+        (deliveryReport) =>
+        {
+            if (deliveryReport.Error.Code != ErrorCode.NoError)
+            {
+                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+            }
+            else
+            {
+                Console.WriteLine($"Produced event to topic {topic}: key = {user,-10} value = {item}");
+                producedMessagesCount += 1;
+            }
+        });
+}
+
+producer.Flush(TimeSpan.FromSeconds(30));
+
+Console.WriteLine($"{producedMessagesCount} messages were produced to topic {topic}");
